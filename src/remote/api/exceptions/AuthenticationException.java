a3ad591b0e4c42d1cd66ec0cdb6dc9e4c2b99c60
johvik <john.viklund@gmail.com>
2014-03-09 11:19:27 +0100

    Improved javadoc for exceptions

--- src/remote/api/exceptions/AuthenticationException.java
+++ src/remote/api/exceptions/AuthenticationException.java
@@ -1,8 +1,20 @@
 package remote.api.exceptions;
 
+/**
+ * Authentication exception, to be used when authentication fails.
+ */
 public class AuthenticationException extends ProtocolException {
+	/**
+	 * Random UID.
+	 */
 	private static final long serialVersionUID = 3577363556401414633L;
 
+	/**
+	 * Constructs a new authentication exception.
+	 * 
+	 * @param message
+	 *            Message of the exception.
+	 */
 	public AuthenticationException(String message) {
 		super(message);
 	}
--- src/remote/api/exceptions/PacketException.java
+++ src/remote/api/exceptions/PacketException.java
@@ -2,13 +2,37 @@ package remote.api.exceptions;
 
 import remote.api.Utils;
 
+/**
+ * Packet exception, mostly for Packet class.
+ */
 public class PacketException extends Exception {
+	/**
+	 * Random UID.
+	 */
 	private static final long serialVersionUID = 1111551034114570738L;
 
+	/**
+	 * Constructs a new packet exception.
+	 * 
+	 * @param message
+	 *            Message of the exception.
+	 * @param data
+	 *            The data that caused the exception.
+	 */
 	public PacketException(String message, byte[] data) {
 		super((message + " " + Utils.toHex(data)).trim());
 	}
 
+	/**
+	 * Constructs a new packet exception.
+	 * 
+	 * @param message
+	 *            Message of the exception.
+	 * @param data
+	 *            The data that caused the exception.
+	 * @param t
+	 *            The Throwable that triggered the exception.
+	 */
 	public PacketException(String message, byte[] data, Throwable t) {
 		super((message + " " + Utils.toHex(data)).trim() + "\n"
 				+ t.getMessage(), t);
